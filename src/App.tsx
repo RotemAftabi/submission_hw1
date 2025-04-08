81c27262202c667903d86dc88b3d1874649f8532
gilad <ggochman@gmail.com>
2025-04-08 20:39:02 +0300

    state update

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,56 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import axios from "axios";
 
 function App() {
-  const [count, setCount] = useState(0)
+  interface Note {
+    id: number;
+    title: string;
+    author: string;
+    content: string;
+  }
+
+  const [count, setCount] = useState(0);
+
+  const NOTES_URL = "http://localhost:3001/notes";
+  const POSTS_PER_PAGE = 10;
+  let activePage = 0;
+
+  const NotesList: React.FC = () => {
+    const [notes, setNotes] = useState<Note[]>([]);
+
+    useEffect(() => {
+      const promise = axios.get(NOTES_URL, {
+        params: {
+          _page: activePage,
+          _per_page: POSTS_PER_PAGE,
+        },
+      });
+      promise
+        .then((response) => {
+          // fill
+          setNotes(response.data);
+        })
+        .catch((error) => {
+          console.log("Encountered an error:" + error);
+        });
+    }, []);
+
+    return (
+      <div>
+        {notes.map((note) => (
+          <div key={note.id} className="note" id={note.id.toString()}>
+            <h2>{note.title}</h2>
+            <small>By {note.author}</small>
+            <br />
+            {note.content}
+          </div>
+        ))}
+      </div>
+    );
+  };
 
   return (
     <>
@@ -29,7 +75,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
