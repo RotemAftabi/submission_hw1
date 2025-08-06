e3041fd910a8ab0a3b9266b8c7067ba927c97a89
gilad <ggochman@gmail.com>
2025-08-06 14:08:29 +0300

    removed notesContext usage (functional again)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,26 +4,19 @@ import Notification from "./components/Notification";
 import Login from "./pages/Login";
 import CreateUser from "./pages/CreateUser";
 import Home from "./pages/Home";
-import { NotesProvider } from "./contexts/NotesContext";
 import { AuthProvider } from "./contexts/AuthContext";
-// import AddNote from "./components/AddNote";
-// import NoteList from "./components/NoteList";
-// import Pagination from "./components/Pagination";
 
 function App() {
   return (
     <AuthProvider>
-      <NotesProvider>
-        <Router>
-          <Notification />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/create-user" element={<CreateUser />} />
-            {/* <Route path="/me" element={<Me />} /> {} */}
-          </Routes>
-        </Router>
-      </NotesProvider>
+      <Router>
+        <Notification />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/create-user" element={<CreateUser />} />
+        </Routes>
+      </Router>
     </AuthProvider>
   );
 }
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,12 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import { NotesProvider } from "./contexts/NotesContext";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <NotesProvider>
-      <App />
-    </NotesProvider>
+    <App />
   </React.StrictMode>
 );
